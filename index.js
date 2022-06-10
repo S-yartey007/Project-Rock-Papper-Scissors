250b782c40c4ed76da148dbde08c43a1ab47b47a
Emmanuel Yartey <supremeconquerorx@gmail.com>
2022-06-10 11:56:52 +0000

    finist the basic operation of the game

--- index.js
+++ index.js
@@ -3,17 +3,59 @@ let random = Math.floor(Math.random() * content.length)
 function computerPlay() {
     return content[random]
 }
-computerPlay()
-const playerSelection = "papper"
-const computerSelection = computerPlay
-function playRound (playerSelection,computerSelection) {
-    if(computerSelection === "rock") {
-        return "You win! Papper beats rock"
-    } else if (computerSelection === "scissors") {
-        return "You lose! Scissors cut papper"
-    } else if (computerSelection === "papper"){
-        return "It's a draw"
-    }
-
+let playerSelection;
+let inputUser;
+function input () {
+     inputUser = prompt("Enter your choice:");
+      playerSelection = inputUser.toLowerCase()
 }
-console.log(playRound(playerSelection,computerSelection))
+
+const computerSelection = computerPlay()
+function playRound (playerSelection,computerSelection) {
+    computerPlay()
+    input()
+    if(computerSelection === playerSelection) {
+        return "Draw"
+    } else if (computerSelection === "scissors" && playerSelection === "papper") {
+        return "You lose!"
+    } else if (computerSelection === "papper" && playerSelection === "scissors"){
+        return "You win!"
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        return "You lose! "
+    } else if(computerSelection === "scissors" && playerSelection === "rock") {
+        return "You win! "
+    } else if(computerSelection === "papper" && playerSelection === "rock") {
+        return "You win!"
+    }else if (computerSelection === "rock" && playerSelection === "papper") {
+        return "You lose"
+    } else {
+        return "Draw"
+    }
+}
+
+let currentRound = playRound(playerSelection,computerSelection)
+let playerScore = 0;
+let computerScore = 0;
+function game() {
+    for(let i = 0; i < 2; i++) {
+        playRound(playerSelection,computerSelection)
+        if(currentRound === "You win!") {
+            playerScore ++;
+        } else if(currentRound === "You lose!") {
+            computerScore ++;
+        } else {
+            playerScore += 0;
+            computerScore += 0;
+        }
+        console.log("Playerscore: " + playerScore)
+    }
+    if(computerScore < playerScore) {
+        console.log(currentRound)
+    } else if( computerScore > playerScore ) {
+        console.log(currentRound)
+    } else {
+        console.log(currentRound)
+    }
+}
+
+game()
